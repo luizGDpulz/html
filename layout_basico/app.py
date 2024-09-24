746f69e23b587fb05abb72e4e8724e883d2466e3
Luiz Gustavo Dias Pulz <162385594+luizGDpulz@users.noreply.github.com>
2024-09-24 23:43:25 +0000

    estrutura ok

--- layout_basico/app.py
+++ layout_basico/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, redirect,  url_for
+from flask import Flask, render_template, request, redirect,  url_for, send_from_directory
 
-app = Flask('__name__')
+app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/images/<path:filename>')
+def server_image(filename):
+    return send_from_directory('./images', filename)
 
 if  __name__ == '__main__':
     #app.run(host="0.0.0.0", port=8000, debug=True)
